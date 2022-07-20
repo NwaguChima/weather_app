0245c8267c18e83f9e46299aa4a2cf92ad9b304e
NwaguChima <nwaguchima48@gmail.com>
2022-07-20 02:23:25 +0100

    added icons to public

--- src/components/current-weather/CurrentWeather.js
+++ src/components/current-weather/CurrentWeather.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./CurrentWeather.css";
 
 const CurrentWeather = () => {
-  return <div>CurrentWeather</div>;
+  return (
+    <div className="weather">
+      <div className="top">
+        <p className="city">Belgrade</p>
+        <p className="weather-description">Sunny</p>
+      </div>
+      <img alt="weather" className="weather-icon" src="" />
+    </div>
+  );
 };
 
 export default CurrentWeather;
