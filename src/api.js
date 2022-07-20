bcc57ca44fad70eca46cb4cec4a5ef070b1c32bc
NwaguChima <nwaguchima48@gmail.com>
2022-07-20 02:04:06 +0100

    setup loadOption function

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "10a63f53d9mshd3a37ee2408afb0p1e06c9jsn64a12027679c",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
